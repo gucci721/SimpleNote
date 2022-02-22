a0fe01937fe79dd0b2a991424c275805a8af7dea
gucci721 <gudraxxtrip@gmail.com>
2022-02-22 15:39:45 +0900

    first commit

---
+++ database/migrations/2022_02_20_143835_create_memos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMemosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('memos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->longText('content');
+            $table->integer('user_id');
+            $table->integer('status')->default('1');
+            //timestampと書いてしまうと、レコード挿入時に更新時に値が入らないためDB::rawで直接書く
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('memos');
+    }
+}
